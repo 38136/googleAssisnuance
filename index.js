3c5a0c89d587ceccb3ce9f0e11faa59a5323764b
38136 <38136.hexaware@gmail.com>
2017-10-13 14:42:03 +0530

    Add all files

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
 var express = require('express');
 var expressApp = express();
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 8080;
 console.log('will see');
 
 exports.nuancetry = (req, res) => {
@@ -14,22 +13,11 @@ exports.nuancetry = (req, res) => {
     });
 
     // Create functions to handle requests here
-
-
-    console.log('will see');
-    console.log(exports.nuancetry);
-
-    function mainIntent(app) {
+ function mainIntent(app) {
         let inputPrompt = app.buildInputPrompt(false,
             'Hi! Say something, and I\'ll repeat it');
         app.ask(inputPrompt);
-    } {
-        /*function mainIntent (app) {
-          let inputPrompt = app.buildInputPrompt(false, 'Hi, I\'m Do It All. You can ask to do this or that. What would you like to do?');
-          app.ask(inputPrompt);
-        }*/
-    }
-
+    } 
     function respond(app) {
         let inputPrompt = app.buildInputPrompt(false,
             'Hi! Say something, and I\'ll repeat it.');
@@ -37,20 +25,23 @@ exports.nuancetry = (req, res) => {
     }
     let actionMap = new Map();
     actionMap.set(app.StandardIntents.MAIN, mainIntent);
-    actionMap.set(app.StandardIntents.TEXT, respond);
+    actionMap.set(app.StandardIntents.TEXT, res);
 
     //add more intents to the map
     app.handleRequest(actionMap);
 }
-expressApp.set('port', port);
-expressApp.use(bodyParser.json({
-    type: 'application/json'
-}));
+  
 
+   
+
+expressApp.use(bodyParser.json());
 expressApp.post('/', function(request,respond)
 {
-    
+
+});
+expressApp.get('/', function(request,respond)
+{
+    respond.send("succes");
 });
 
-expressApp.listen(port);
-console.log('Parrot Assistant listening on port %s', port);
+expressApp.listen(3000);
