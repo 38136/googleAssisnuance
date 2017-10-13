4cc999cafcaa298146bf7dbfa09bf995801d5c5b
38136 <38136.hexaware@gmail.com>
2017-10-13 14:21:50 +0530

    Add all files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+console.log('will see');
 
-exports.nuancetry= (req, res) => {
+exports.nuancetry = (req, res) => {
     const app = new ActionsSdkApp({
         request: req,
         response: res
@@ -10,29 +11,30 @@ exports.nuancetry= (req, res) => {
 
     // Create functions to handle requests here
 
+
+    console.log('will see');
+    console.log(exports.nuancetry);
+
+    function mainIntent(app) {
+        let inputPrompt = app.buildInputPrompt(false,
+            'Hi! Say something, and I\'ll repeat it');
+        app.ask(inputPrompt);
+    } {
+        /*function mainIntent (app) {
+          let inputPrompt = app.buildInputPrompt(false, 'Hi, I\'m Do It All. You can ask to do this or that. What would you like to do?');
+          app.ask(inputPrompt);
+        }*/
+    }
+
+    function respond(app) {
+        let inputPrompt = app.buildInputPrompt(false,
+            'Hi! Say something, and I\'ll repeat it.');
+        app.ask(inputPrompt);
+    }
+    let actionMap = new Map();
+    actionMap.set(app.StandardIntents.MAIN, mainIntent);
+    actionMap.set(app.StandardIntents.TEXT, respond);
+
+    //add more intents to the map
+    app.handleRequest(actionMap);
 }
-console.log('will see');
-console.log(exports.nuancetry);
-
-function mainIntent(app) {
-    let inputPrompt = app.buildInputPrompt(false,
-        'Hi! Say something, and I\'ll repeat it');
-    app.ask(inputPrompt);
-}
-{/*function mainIntent (app) {
-  let inputPrompt = app.buildInputPrompt(false, 'Hi, I\'m Do It All. You can ask to do this or that. What would you like to do?');
-  app.ask(inputPrompt);
-}*/}
-function respond(app) {
-    let inputPrompt = app.buildInputPrompt(false,
-        'Hi! Say something, and I\'ll repeat it.');
-    app.ask(inputPrompt);
-}
-let actionMap = new Map();
-actionMap.set(app.StandardIntents.MAIN, mainIntent);
-actionMap.set(app.StandardIntents.TEXT, respond);
-
-//add more intents to the map
-
-
-app.handleRequest(actionMap);
